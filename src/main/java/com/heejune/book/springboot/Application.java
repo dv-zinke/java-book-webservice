d72324f2c84ebc1577441bcb7c0d0b013df0c8c6
hj <dv.zinke@gmail.com>
2020-02-02 13:08:25 +0900

    등록일 수정일 추가

--- src/main/java/com/heejune/book/springboot/Application.java
+++ src/main/java/com/heejune/book/springboot/Application.java
@@ -4,8 +4,10 @@ package com.heejune.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
